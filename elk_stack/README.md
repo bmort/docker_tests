9b7eb1b89be710a55cbcb3dd1da734921d690554
Benjamin Mort <ben.mort@gmail.com>
2017-09-18 14:31:16 +0100

    added elk stack test

---
+++ elk_stack/README.md
@@ -0,0 +1,5 @@
+# References
+
+- <https://botleg.com/stories/log-management-of-docker-swarm-with-elk-stack>
+- <https://github.com/botleg/swarm-logging>
+- <https://botleg.com/stories/monitoring-docker-swarm-with-cadvisor-influxdb-and-grafana/>
---
+++ elk_stack/test_app.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+"""Dummy app to spam a bunch of logs"""
+import time
+import random
+import socket
+
+
+def main():
+    """."""
+    host_name = socket.gethostname()
+    start_time = time.time()
+    try:
+        while True:
+            elapsed = time.time() - start_time
+            print('hello from %s (%.1f s)' % (host_name, elapsed))
+            time.sleep(random.uniform(0.01, 0.5))
+    except KeyboardInterrupt:
+        print('Exiting...')
+
+
+if __name__ == '__main__':
+    main()
